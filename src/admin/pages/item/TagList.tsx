9a81ad6720ae0e1a87e06e8015b59be857697420
Jin Young Park <jy2045@sogang.ac.kr>
2020-10-21 17:30:07 +0900

    ssho-lab/ssho-issue-board#26 태그수정 버튼 추가
    * 기존 태그리스트와 다를 시 버튼 활성화 (buttonClickable state 활용)

--- src/admin/pages/item/TagList.tsx
+++ src/admin/pages/item/TagList.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import {Select} from 'antd';
-
+import React, {useState} from 'react';
+import {Select, Button} from 'antd';
 const {Option, OptGroup} = Select;
 
 interface Tag{
@@ -23,7 +22,7 @@ interface TagListProps{
   
 const TagList = ({tagListPerItem, allTagList}: TagListProps) => {
   const realTagList = tagListPerItem && Array.from(new Set<string>(tagListPerItem.map(el => el.realTagList).flat().map(tag=>tag.name)));
-  
+
   const options= allTagList.map(el => {
     return (
       <OptGroup label={el.expTag.name}>
@@ -32,8 +31,21 @@ const TagList = ({tagListPerItem, allTagList}: TagListProps) => {
     )
   })
 
+  const [buttonClickable, setButtonClickable] = useState<boolean>(false);
+
+  const handleSelectChange = (selected: string[]) => {
+    if(JSON.stringify(selected.sort()) === JSON.stringify(realTagList.sort())){
+      setButtonClickable(false);
+      return;
+    }
+    setButtonClickable(true);
+  }
+
   return (
-    <Select mode="multiple" defaultValue={realTagList} style={{width: '100%'}}  placeholder="태그 리스트" optionLabelProp="label">{options}</Select>
+    <div style={{display: 'flex'}}>
+      <Select mode="multiple" defaultValue={realTagList} style={{width: '80%'}}  placeholder="태그 리스트" optionLabelProp="label" onChange={handleSelectChange}>{options}</Select>
+      <Button type="primary" style={{width: '20%'}} disabled={!buttonClickable}>태그 수정</Button>
+    </div>
   );
 }
 
