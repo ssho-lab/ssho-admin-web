eec77110d68151a2f14bc72192a9f91a4c17e1c8
Jin Young Park <jy2045@sogang.ac.kr>
2020-10-23 16:47:40 +0900

    ssho-lab/ssho-issue-board#26 태그 수정 UI 구현
    * 기존 tagList와 비교하여 다를시 버튼 활성화
    * 태그 수정 버튼 클릭시 API 요청을 보내고 성공시 메시지 출력
    * 기존 tagList를 수정된 tagList로 바꾸고 버튼 비활성화`

--- src/admin/pages/item/TagList.tsx
+++ src/admin/pages/item/TagList.tsx
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
-import {Select, Button} from 'antd';
+import {Select, Button, message} from 'antd';
+import axios from 'axios';
+import API_ENDPOINTS from '../../../endpoints';
+
 const {Option, OptGroup} = Select;
 
 interface Tag{
@@ -16,35 +19,62 @@ interface TagProps{
 interface TagListProps{
   tagListPerItem: TagProps[];
   allTagList: TagProps[];
+  itemId: string;
 }
 
+const updateTag = (itemId: string, data: any) => {
+  return axios.post(API_ENDPOINTS.ITEM_API+'/item/update/tag',data, {
+    params:{
+      itemId
+    }
+  })
+}
 
-  
-const TagList = ({tagListPerItem, allTagList}: TagListProps) => {
-  const realTagList = tagListPerItem && Array.from(new Set<string>(tagListPerItem.map(el => el.realTagList).flat().map(tag=>tag.name)));
-
+const TagList = ({itemId, tagListPerItem, allTagList}: TagListProps) => {
+  let defaultTagList = tagListPerItem && Array.from(new Set<string>(tagListPerItem.map(el => el.realTagList).flat().map(tag=> tag.name)))
   const options= allTagList.map(el => {
     return (
       <OptGroup label={el.expTag.name}>
-        {el.realTagList.map(realTag => <Option value={realTag.name}>{realTag.name}</Option>)}
+        {el.realTagList.map(realTag => <Option label={el.expTag.name} value={realTag.name}>{realTag.name}</Option>)}
       </OptGroup>
     )
   })
 
   const [buttonClickable, setButtonClickable] = useState<boolean>(false);
+  const [expTagList, setExpTagList] = useState<string[]>([]);
+  const [realTagList, setRealTagList] = useState<string[]>([]);
 
-  const handleSelectChange = (selected: string[]) => {
-    if(JSON.stringify(selected.sort()) === JSON.stringify(realTagList.sort())){
+  const handleSelectChange = (selected: string[], options: any) => {
+    setExpTagList(Array.from(new Set<string>(options.map((option: any) => option.label))));
+    setRealTagList(options.map((option: any) => option.value));
+
+    if(JSON.stringify(selected.sort()) === JSON.stringify(defaultTagList.sort())){
       setButtonClickable(false);
       return;
     }
     setButtonClickable(true);
   }
 
+  const handleClick = () => {
+    let newTagList = allTagList.filter(el => expTagList.includes(el.expTag.name));
+    newTagList.forEach(el => el.realTagList = el.realTagList.filter(realTag => realTagList.includes(realTag.name)));
+
+
+    updateTag(itemId, newTagList)
+      .then(function (response: any) {
+        message.success('태그 수정 완료');
+        setButtonClickable(false);
+        defaultTagList = [...realTagList];
+      })
+      .catch(function (err){
+        message.error('태그 수정 실패');
+      })
+  }
+
   return (
     <div style={{display: 'flex'}}>
-      <Select mode="multiple" defaultValue={realTagList} style={{width: '80%'}}  placeholder="태그 리스트" optionLabelProp="label" onChange={handleSelectChange}>{options}</Select>
-      <Button type="primary" style={{width: '20%'}} disabled={!buttonClickable}>태그 수정</Button>
+      <Select mode="multiple" allowClear defaultValue={defaultTagList} style={{width: '80%'}}  placeholder="태그 리스트" onChange={handleSelectChange}>{options}</Select>
+      <Button type="primary" style={{width: '20%'}} disabled={!buttonClickable} onClick={handleClick}>태그 수정</Button>
     </div>
   );
 }
