0a6d6f7c567ed5305a040a6a91f3db1fa08721b8
Jin Young Park <jy2045@sogang.ac.kr>
2020-11-19 00:30:05 +0900

    ssho-lab/ssho-issue-board#32 - 상품 태그 조회 및 업데이트 로직 변경

--- src/admin/pages/item/TagList.tsx
+++ src/admin/pages/item/TagList.tsx
@@ -3,23 +3,17 @@ import {Select, Button, message} from 'antd';
 import axios from 'axios';
 import API_ENDPOINTS from '../../../endpoints';
 
-const {Option, OptGroup} = Select;
+const {Option} = Select;
 
 interface Tag{
   id: string;
   name: string;
-  level: number;
-  parentTagId: string;
 }
 
-interface TagGroup{
-  lvl1Tag: Tag;
-  lvl2Tag: Tag[];
-}
 
 interface TagListProps{
   tagListPerItem: Tag[] | null;
-  allTagList: TagGroup[];
+  allTagList: Tag[];
   itemId: string;
 }
 
@@ -34,50 +28,36 @@ const updateTag = (itemId: string, data: any) => {
 const TagList = ({itemId, tagListPerItem, allTagList}: TagListProps) => {
   let defaultTagList: any[] | undefined = tagListPerItem===null ? undefined : tagListPerItem.map(el => el.name)
 
-  const options= allTagList.map(el => {
+  const options= allTagList.map(tag => {
     return (
-      <OptGroup label={el.lvl1Tag.name}>
-        <Option style={{fontSize: '10px'}} label={el.lvl1Tag.id} value={el.lvl1Tag.name} isParent={true}>{el.lvl1Tag.name}</Option>
-        {el.lvl2Tag.length=== 0 ? null : el.lvl2Tag.map(tag => <Option style={{fontSize: '10px'}} label={tag.id} value={tag.name} isParent={false}>{tag.name}</Option>)}
-      </OptGroup>
+        <Option style={{fontSize: '10px'}} label={tag.id} value={tag.name}>{tag.name}</Option>
     )
   })
 
   const [buttonClickable, setButtonClickable] = useState<boolean>(false);
-  const [level1TagList, setLevel1TagList] = useState<string[]>([]);
-  const [level2TagList, setLevel2TagList] = useState<string[]>([]);
+  const [newTagList, setNewTagList] = useState<string[]>([]);
 
   const handleSelectChange = (selected: string[], options: any) => {
 
-    options.forEach((option: any) => {
-      if(option.isParent) setLevel1TagList([...level1TagList,option.value]);
-      else setLevel2TagList([...level2TagList, option.value]);
-    })
-
+    setNewTagList(options.map((option:any) => option.value));
 
     if(JSON.stringify(selected.sort()) === JSON.stringify(defaultTagList && defaultTagList.sort())){
       setButtonClickable(false);
       return;
     }
+
     setButtonClickable(true);
   }
 
   const handleClick = () => {
-    let newTagList = allTagList.filter(el => level1TagList.includes(el.lvl1Tag.name));
-    newTagList.forEach(el => el.lvl2Tag = el.lvl2Tag.filter(tag => level2TagList.includes(tag.name)));
-
-    let res:any = []
-
-    newTagList.forEach(el => {
-      res.push(el.lvl1Tag)
-      el.lvl2Tag.forEach(e => res.push(e))
-    })
-
-    updateTag(itemId, res)
+    
+    const data = allTagList.filter(tag => newTagList.includes(tag.name));
+    
+    updateTag(itemId, data)
       .then(function (response: any) {
         message.success('태그 수정 완료');
         setButtonClickable(false);
-        defaultTagList = [...level1TagList, ...level2TagList];
+        defaultTagList = newTagList;
       })
       .catch(function (err: any){
         message.error('태그 수정 실패');
